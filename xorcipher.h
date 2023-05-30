4eff63e9134749a3174ac84345faf22b538c30ea
Jan Maes <janmaes@subadmins-iMac-2850.local>
2023-05-30 22:11:21 +0200

    comments

--- xorcipher.h
+++ xorcipher.h
@@ -1,17 +1,23 @@
+/* xorcipher - v1.0 - MIT License - https://github.com/janm31415/XorCipher
+ 
+
+   Do this:
+   #define XORCIPHER_IMPLEMENTATION
+   before you include this file in *one* C or C++ file to create the implementation.
+   
+   // i.e. it should look like this:
+   #include ...
+   #include ...
+   #include ...
+   #define XORCIPHER_IMPLEMENTATION
+   #include "xorcipher.h"
+   
+   You can #define XORCIPHER_MALLOC and XORCIPHER_FREE if you don't want to use malloc and free.
+   
+ */
+ 
 #ifndef XORCIPHER_INCLUDE_H
 #define XORCIPHER_INCLUDE_H
-/*
- Do this:
- #define XORCIPHER_IMPLEMENTATION
- before you include this file in *one* C or C++ file to create the implementation.
- 
- // i.e. it should look like this:
- #include ...
- #include ...
- #include ...
- #define XORCIPHER_IMPLEMENTATION
- #include "xorcipher.h"
- */
 
 #include <stdlib.h>
 #include <string.h>
@@ -28,8 +34,28 @@ extern "C" {
 #endif
 #endif
 
+/*
+Base64 encoding to encode a buffer of bytes of a given length.
+The return value of this function is the base64 encoded 0-terminated string.
+The string is allocated with XORCIPHER_MALLOC. It is the responsability of the
+user to free the memory with XORCIPHER_FREE.
+*/
 XORCIPHER_DEF char* xorcipher_base64_encode(const unsigned char* bytes, unsigned long length);
+
+/*
+Base64 decoding of a 0-terminated base64 encoded string. The return value of this function is
+the decoded binary buffer. The size of the decoded binary buffer is returned via the parameter
+decoded_size. The output buffer is allocated with XORCIPHER_MALLOC. It is the responsability of
+the user to free the memory with XORCIPHER_FREE.
+*/
 XORCIPHER_DEF unsigned char* xorcipher_base64_decode(unsigned long* decoded_size, const char* encoded_string);
+
+/*
+Given an input buffer message and corresponding length message_length, this function will XOR all bytes
+of the input buffer with the bytes of base64 decoded password. Calling the method once will encrypt the
+input buffer message. Calling this method a second time with the same parameters will decrypt the input
+buffer message.
+*/
 XORCIPHER_DEF void xorcipher_xor(unsigned char* message, unsigned long message_length, const char* base64_password);
 
 #ifdef __cplusplus
@@ -46,10 +72,6 @@ XORCIPHER_DEF void xorcipher_xor(unsigned char* message, unsigned long message_l
 
 #ifndef XORCIPHER_FREE
 #  define XORCIPHER_FREE(x) free(x)
-#endif
-
-#ifndef XORCIPHER_REALLOC
-#  define XORCIPHER_REALLOC(x, size) realloc(x, size)
 #endif
 
 static const char base64_chars[65] =
